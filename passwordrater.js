f16e9c5bed72f548b5ca8cf86a691fac152ed9dc
james <jnguy2018@gmail.com>
2022-05-05 01:34:22 -0700

    Connected html and JS

--- passwordrater.js
+++ passwordrater.js
@@ -1,45 +1,49 @@
-const fs = require('fs');
-const readline = require('readline');
+const passwordRaterInput = document.getElementById('password-rater-input');
+const passwordRaterForm = document.getElementById('password-rater-form');
+const passwordRaterOutput = document.getElementById('password-rater-output');
 
-const getWords = async() => {
-  const fileStream = fs.createReadStream('words.txt');
+// On submission of form, do password checking logic
+passwordRaterForm.addEventListener('submit', e => {
+  e.preventDefault();
+  passwordRaterOutput.innerHTML = checkPassword(passwordRaterInput.value);
+});
 
-  const rl = readline.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity
-  });
-
-  let words = [];
-  for await (const line of rl) {
-    if (line.length > 3) {
-      words.push(line)
-    }
-  };
-
+const checkPassword = (password) => {
+  let words = wordList                              // create a copy of wordList
   words = words.sort((a,b) => b.length - a.length); // sort words so longest words come first
-  return words;
-}
+  let passwordStrength = "";                                 // final message to output to client
+  let additionalMessage = null;
+  let wordsFound = 0;                               // increment this when a word is found to add to the final calculation of num guesses
+  const availableChars = 52;                        // alphabetic
 
-const checkPassword = async(password) => {
-  const availableChars = 52; // alphabetic
-  const words = await getWords();
-  let wordsFound = 0;
-
+  // for each word, check if it's in password
+  // then remove it from the pass and add to words found
   words.forEach((word) => {
-    while (password.includes(word)) {
+    while (password.toLowerCase().includes(word)) {
       wordsFound++;
-      password = password.replace(word, '');
+      var regEx = new RegExp(word, "ig");
+      password = password.replace(regEx, '&');
       console.log("Password contained a word!");
     }
   })
 
-  const guesses = Math.pow(availableChars, wordsFound + password.length) / 2;
+  // if password contains words & is weak/medium strength
+  // inform the user
+  if (wordsFound > 0) additionalMessage = `Your password contains ${wordsFound} word(s).`;
+  
+  // number of guesses is equal to available chars (f.e. 52 for alphabetic) to the power of
+  // words found in password + the rest of the password
+  // all divided by 2, because it takes about half num of guesses to crack pass
+  const guesses = Math.pow(availableChars, password.length - wordsFound) / 2;
   if (guesses < Math.pow(10, 6))
-    console.log("weak pass");
+    passwordStrength = "Weak password";
   else if (guesses < Math.pow(10, 14))
-    console.log("medium pass");
+    passwordStrength = "Medium password";
   else
-    console.log("strong pass");
+    passwordStrength = "Strong password";
+  
+  if (additionalMessage != null && passwordStrength != "Strong password") {
+    passwordStrength += ". " + additionalMessage;
+  }
+  return passwordStrength;
 }
-
-checkPassword("hellohello");
